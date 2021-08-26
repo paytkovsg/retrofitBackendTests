c41825cb916c3388e851e8866b9ed110d402f846
paytkovsg <paytkov.s@gmail.com>
2021-08-26 22:00:14 +0500

    ДЗ5

---
+++ src/main/java/service/CategoryService.java
@@ -0,0 +1,13 @@
+package service;
+
+import dto.Category;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import dto.Category;
+
+public interface CategoryService {
+    @GET("categories/{id}")
+    Call<Category> getCategory(@Path("id") Integer id) ;
+}
